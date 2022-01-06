2fc7d965584b680a3313e007efefdb6397108f21
Siobhán O'Hanlon <Siobhanhanlon@outlook.com>
2022-01-06 00:58:59 +0000

    Have Colour Pins Moving

--- Mastermind/Mastermind/MainPage.xaml.cs
+++ Mastermind/Mastermind/MainPage.xaml.cs
@@ -10,12 +10,17 @@ namespace Mastermind
 {
     public partial class MainPage : ContentPage
     {
+        #region GlobalVariables
         //Constants
         const int NUM_ROW = 11, NUM_COL = 4; //Guess Area
         const int COR_ROW = 30, COR_COL = 2; //Correct Display
 
-        Image imagePinSender;
-        int tapCount = 1;
+        //BoxView
+        BoxView currPinSelected;
+
+        //Global Vraible for Round
+        int round = 10;
+        #endregion
 
         //Main
         public MainPage()
@@ -28,32 +33,52 @@ namespace Mastermind
         private void SetUpBoard()
         {
             //Declare Variables
-            Image guess;
             Image guessSelect;
 
-            //Empty Guess Area
-            //Tap Gesture Recongizer
-            TapGestureRecognizer tap = new TapGestureRecognizer();
-            tap.NumberOfTapsRequired = 1;
-            tap.Tapped += Square_Tapped;
+            #region AnswerArea
+            //<!-- Answer Area -->
+            // < Image Source = "Images/QuestionMark.png" Grid.Column = "0" Grid.Row = "0" />
+            //< Image Source = "Images/QuestionMark.png" Grid.Column = "1" Grid.Row = "0" />
+            //< Image Source = "Images/QuestionMark.png" Grid.Column = "2" Grid.Row = "0" />
+            //< Image Source = "Images/QuestionMark.png" Grid.Column = "3" Grid.Row = "0" />
 
-            //Add Image for each row & column needed
+            #endregion
+
+            #region SetUpGuessArea-Working
+            //Tap Gesture Recongizer
+            TapGestureRecognizer emptyTap = new TapGestureRecognizer();
+            emptyTap.NumberOfTapsRequired = 1;
+            emptyTap.Tapped += Empty_Tapped;//squaretapped
+
+            //Add Boxview for each row & column needed
             for (int r = 1; r < NUM_ROW; r++)
             {
                 for (int c = 0; c < NUM_COL; c++)
                 {
-                    //Create new Image
-                    guess = new Image();
-                    guess.Source = "Images/Empty.png";
+                    //Create new BoxView
+                    BoxView emptyGuess;
+                    emptyGuess = new BoxView();
+                    emptyGuess.BackgroundColor = Color.SaddleBrown;
+                    emptyGuess.StyleId = "GuessArea";
+                    emptyGuess.HorizontalOptions = LayoutOptions.Center;
+                    emptyGuess.VerticalOptions = LayoutOptions.Center;
+                    emptyGuess.HeightRequest = 40;
+                    emptyGuess.WidthRequest = 40;
+                    emptyGuess.CornerRadius = 40;
 
                     //Set Value
-                    guess.SetValue(Grid.RowProperty, r);
-                    guess.SetValue(Grid.ColumnProperty, c);
+                    emptyGuess.SetValue(Grid.RowProperty, r);
+                    emptyGuess.SetValue(Grid.ColumnProperty, c);
+
+                    //Add Tap Gesture
+                    emptyGuess.GestureRecognizers.Add(emptyTap);
 
                     //Add to Children
-                    GrdGameLayout.Children.Add(guess);
+                    GrdGameLayout.Children.Add(emptyGuess);
                 }
             }
+            #endregion
+
             #region CheckIfCorrectGuess
 
             //Set up Board in xaml
@@ -97,125 +122,280 @@ namespace Mastermind
                 }
             }
             #endregion
+
+            #region CreateColourPins
+            //Tap Gesture Recongizer
+            TapGestureRecognizer pinTap = new TapGestureRecognizer();
+            pinTap.NumberOfTapsRequired = 1;
+            pinTap.Tapped += PinTapped;
+            
+            #region Red
+            //Create new BoxView
+            BoxView redPin;
+            redPin = new BoxView();
+            redPin.BackgroundColor = Color.Red;
+            redPin.StyleId = "RedPin";
+            redPin.HorizontalOptions = LayoutOptions.Center;
+            redPin.VerticalOptions = LayoutOptions.Center;
+            redPin.HeightRequest = 40;
+            redPin.WidthRequest = 40;
+            redPin.CornerRadius = 40;
+
+            //Set Value
+            redPin.SetValue(Grid.RowProperty, 12);
+            redPin.SetValue(Grid.ColumnProperty, 0);
+
+            //Add Gesture Recognizer
+            redPin.GestureRecognizers.Add(pinTap);
+
+            //Add to Children
+            GrdGameLayout.Children.Add(redPin);
+            #endregion
+
+            #region Green
+            //Create new BoxView
+            BoxView greenPin;
+            greenPin = new BoxView();
+            greenPin.BackgroundColor = Color.Green;
+            greenPin.StyleId = "GreenPin";
+            greenPin.HorizontalOptions = LayoutOptions.Center;
+            greenPin.VerticalOptions = LayoutOptions.Center;
+            greenPin.HeightRequest = 40;
+            greenPin.WidthRequest = 40;
+            greenPin.CornerRadius = 40;
+
+            //Set Value
+            greenPin.SetValue(Grid.RowProperty, 12);
+            greenPin.SetValue(Grid.ColumnProperty, 1);
+
+            //Add Gesture Recognizer
+            greenPin.GestureRecognizers.Add(pinTap);
+
+            //Add to Children
+            GrdGameLayout.Children.Add(greenPin);
+            #endregion
+            
+            #region Blue
+            //Create new BoxView
+            BoxView bluePin;
+            bluePin = new BoxView();
+            bluePin.BackgroundColor = Color.Blue;
+            bluePin.StyleId = "BluePin";
+            bluePin.HorizontalOptions = LayoutOptions.Center;
+            bluePin.VerticalOptions = LayoutOptions.Center;
+            bluePin.HeightRequest = 40;
+            bluePin.WidthRequest = 40;
+            bluePin.CornerRadius = 40;
+
+            //Set Value
+            bluePin.SetValue(Grid.RowProperty, 12);
+            bluePin.SetValue(Grid.ColumnProperty, 2);
+
+            //Add Gesture Recognizer
+            bluePin.GestureRecognizers.Add(pinTap);
+
+            //Add to Children
+            GrdGameLayout.Children.Add(bluePin);
+            #endregion
+            
+            #region Black
+            //Create new BoxView
+            BoxView blackPin;
+            blackPin = new BoxView();
+            blackPin.BackgroundColor = Color.Black;
+            blackPin.StyleId = "BlackPin";
+            blackPin.HorizontalOptions = LayoutOptions.Center;
+            blackPin.VerticalOptions = LayoutOptions.Center;
+            blackPin.HeightRequest = 40;
+            blackPin.WidthRequest = 40;
+            blackPin.CornerRadius = 40;
+
+            //Set Value
+            blackPin.SetValue(Grid.RowProperty, 12);
+            blackPin.SetValue(Grid.ColumnProperty, 3);
+
+            //Add Gesture Recognizer
+            blackPin.GestureRecognizers.Add(pinTap);
+
+            //Add to Children
+            GrdGameLayout.Children.Add(blackPin);
+            #endregion
+            
+            #region White
+            //Create new BoxView
+            BoxView whitePin;
+            whitePin = new BoxView();
+            whitePin.BackgroundColor = Color.White;
+            whitePin.StyleId = "WhitePin";
+            whitePin.HorizontalOptions = LayoutOptions.Center;
+            whitePin.VerticalOptions = LayoutOptions.Center;
+            whitePin.HeightRequest = 40;
+            whitePin.WidthRequest = 40;
+            whitePin.CornerRadius = 40;
+
+            //Set Value
+            whitePin.SetValue(Grid.RowProperty, 13);
+            whitePin.SetValue(Grid.ColumnProperty, 0);
+
+            //Add Gesture Recognizer
+            whitePin.GestureRecognizers.Add(pinTap);
+
+            //Add to Children
+            GrdGameLayout.Children.Add(whitePin);
+            #endregion
+            
+            #region Purple
+            //Create new BoxView
+            BoxView purplePin;
+            purplePin = new BoxView();
+            purplePin.BackgroundColor = Color.Purple;
+            purplePin.StyleId = "PurplePin";
+            purplePin.HorizontalOptions = LayoutOptions.Center;
+            purplePin.VerticalOptions = LayoutOptions.Center;
+            purplePin.HeightRequest = 40;
+            purplePin.WidthRequest = 40;
+            purplePin.CornerRadius = 40;
+
+            //Set Value
+            purplePin.SetValue(Grid.RowProperty, 13);
+            purplePin.SetValue(Grid.ColumnProperty, 1);
+
+            //Add Gesture Recognizer
+            purplePin.GestureRecognizers.Add(pinTap);
+
+            //Add to Children
+            GrdGameLayout.Children.Add(purplePin);
+            #endregion
+            
+            #region Yellow
+            //Create new BoxView
+            BoxView yellowPin;
+            yellowPin = new BoxView();
+            yellowPin.BackgroundColor = Color.Yellow;
+            yellowPin.StyleId = "YellowPin";
+            yellowPin.HorizontalOptions = LayoutOptions.Center;
+            yellowPin.VerticalOptions = LayoutOptions.Center;
+            yellowPin.HeightRequest = 40;
+            yellowPin.WidthRequest = 40;
+            yellowPin.CornerRadius = 40;
+
+            //Set Value
+            yellowPin.SetValue(Grid.RowProperty, 13);
+            yellowPin.SetValue(Grid.ColumnProperty, 2);
+
+            //Add Gesture Recognizer
+            yellowPin.GestureRecognizers.Add(pinTap);
+
+            //Add to Children
+            GrdGameLayout.Children.Add(yellowPin);
+            #endregion
+            
+            #region Orange
+            //Create new BoxView
+            BoxView orangePin;
+            orangePin = new BoxView();
+            orangePin.BackgroundColor = Color.Orange;
+            orangePin.StyleId = "OrangePin";
+            orangePin.HorizontalOptions = LayoutOptions.Center;
+            orangePin.VerticalOptions = LayoutOptions.Center;
+            orangePin.HeightRequest = 40;
+            orangePin.WidthRequest = 40;
+            orangePin.CornerRadius = 40;
+
+            //Set Value
+            orangePin.SetValue(Grid.RowProperty, 13);
+            orangePin.SetValue(Grid.ColumnProperty, 3);
+
+            //Add Gesture Recognizer
+            orangePin.GestureRecognizers.Add(pinTap);
+
+            //Add to Children
+            GrdGameLayout.Children.Add(orangePin);
+            #endregion
+            #endregion
+
+            //Clear Selected
+            currPinSelected = null;
         }
 
         //Colour Pins Tapped
-        private void ColourPinsTapped(object sender, EventArgs e)
+        #region ColourPinTapped
+        private void PinTapped(object sender, EventArgs e)
         {
-            //Update Counter
-            tapCount++;
+            BoxView currPiece = (BoxView)sender;
 
-            //Declare Variables
-            imagePinSender = (Image)sender;
-            int selectedColour;
+            SelectThisColour(currPiece);
+        }
 
-            switch (imagePinSender.StyleId)
+        //
+        private void SelectThisColour(BoxView thisOne)
+        {
+            //Select Piece
+            if (currPinSelected == null)
             {
-                case "RedDot":
-                    selectedColour = 1;
-                    break;
+                //This Piece is Selected
+                currPinSelected = thisOne;
 
-                case "GreenDot":
-                    selectedColour = 2;
-                    break;
-
-                case "BlueDot":
-                    selectedColour = 3;
-                    break;
-
-                case "BlackDot":
-                    selectedColour = 4;
-                    break;
-
-                case "WhiteDot":
-                    selectedColour = 5;
-                    break;
-
-                case "TanDot":
-                    selectedColour = 6;
-                    break;
-
-                case "YellowDot":
-                    selectedColour = 7;
-                    break;
-
-                case "OrangeDot":
-                    selectedColour = 8;
-                    break;
-
-                default:
-                    selectedColour = 0;
-                    break;
+                //Change Opacity
+                currPinSelected.Opacity = 0.7;
             }
 
-            if(tapCount%2 > 0)
-            {
-                imagePinSender.Opacity = 0.5;
-            }
+            //Deselect
             else
             {
-                foreach(var piece in GrdGameLayout.Children)
-                {
-                    if(piece.StyleId.Contains("Dot"))
-                    {
-                        piece.Opacity = 1;
-                    }
-                }
+                //Deselecting Piece
+                currPinSelected = null;
+
+                //Reset Piece Selected
+                ResetCurrentSelectedPin();
             }
-            
         }
+
+        #region Working
+        //Move Colour Pin to Guess
+        private void MoveColourPinto(int destRow, int destCol)
+        {
+            if (destRow == round)
+            {
+                currPinSelected.SetValue(Grid.RowProperty, destRow);
+                currPinSelected.SetValue(Grid.ColumnProperty, destCol);
+            }
+
+            ResetCurrentSelectedPin();
+        }
+        #endregion
+
+        //Square Tapped
+        private void Empty_Tapped(object sender, EventArgs e)
+        {
+            //Select Place to move to
+            BoxView currSq = (BoxView)sender;
+
+            //Where Placing Pin
+            MoveColourPinto((int)currSq.GetValue(Grid.RowProperty),
+                          (int)currSq.GetValue(Grid.ColumnProperty));
+        }
+        
+        private void ColourPinsTapped(object sender, EventArgs e)
+        {
+        }
+        #endregion
+
+        //Reset Current Colour
+        private void ResetCurrentSelectedPin()
+        {
+            //If no Piece Selected
+            if (currPinSelected == null) return;
+
+            //If Piece Selected
+            currPinSelected.Opacity = 1;
+        }//End ResetCurrrentPiece
+
 
         //
         private void Answer(int selectedColour)
         {
             var answer = new Random();
-        }
-
-        //Square Tapped
-        private void Square_Tapped(object sender, EventArgs e)
-        {
-            //Declare Variables
-            int selectedColour;
-            var imageSender = (Image)sender;
-
-            switch (imagePinSender.StyleId)
-            {
-                case "RedDot":
-                    selectedColour = 1;
-                    break;
-
-                case "GreenDot":
-                    selectedColour = 2;
-                    break;
-
-                case "BlueDot":
-                    selectedColour = 3;
-                    break;
-
-                case "BlackDot":
-                    selectedColour = 4;
-                    break;
-
-                case "WhiteDot":
-                    selectedColour = 5;
-                    break;
-
-                case "TanDot":
-                    selectedColour = 6;
-                    break;
-
-                case "YellowDot":
-                    selectedColour = 7;
-                    break;
-
-                case "OrangeDot":
-                    selectedColour = 8;
-                    break;
-
-                default:
-                    selectedColour = 0;
-                    break;
-            }
         }
     }
 }
